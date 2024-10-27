0397b67f23779354593c534fc326e63b70e85b15
vitormoscoso <vitormoscoso@gmail.com>
2024-10-27 11:37:11 -0300

    Refactor functions.ts to include reconstructBase64String function

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,17 +1,22 @@
-const functions = () =>{
-    const findValueByField = (data: any[], searchVar: string, field: string, filter: string) => {
-        if (data) {
-            const searchItem = data.find((item) => item[filter] === searchVar);
-            if (searchItem) {
-                return searchItem[field];
-            }
-        }
-        return '';
-    };
-    
-    return {
-        findValueByField,
-    }
-}
+const functions = () => {
+	const findValueByField = (data: any[], searchVar: string, field: string, filter: string) => {
+		if (data) {
+			const searchItem = data.find((item) => item[filter] === searchVar);
+			if (searchItem) {
+				return searchItem[field];
+			}
+		}
+		return '';
+	};
+
+	const reconstructBase64String = (base64Part: string): string => {
+		return `data:image/jpeg;base64,${base64Part}`;
+	};
+
+	return {
+		findValueByField,
+        reconstructBase64String,
+	};
+};
 
 export default functions;
